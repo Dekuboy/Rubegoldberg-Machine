04df27efa6f0412296c2bd159e8f409c11f664fa
George Jeneson <s5009412@bournemouth.ac.uk>
2018-05-17 18:16:23 +0100

    Gear Face

---
+++ Rubegoldberg Machine/Assets/Scripts/GearSpin.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GearSpin : MonoBehaviour {
+
+    public float rotationSpeed;
+    public bool rotationTrigger = false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (rotationTrigger)
+        {
+            transform.Rotate(0, rotationSpeed, 0);
+        }
+	}
+}
---
+++ Rubegoldberg Machine/Assets/Scripts/GearTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GearTrigger : MonoBehaviour {
+
+    public GameObject Gear0;
+    public GameObject Gear1;
+    public GameObject Gear2;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Gear0.GetComponent<GearSpin>().rotationTrigger = true;
+        Gear1.GetComponent<GearSpin>().rotationTrigger = true;
+        Gear2.GetComponent<GearSpin>().rotationTrigger = true;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
