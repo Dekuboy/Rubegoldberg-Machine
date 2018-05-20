46d2b92f57c0f4e2ace533704f919b6d0fae24a1
George Jeneson <s5009412@bournemouth.ac.uk>
2018-05-20 12:34:38 +0100

    Finished

---
+++ Rubegoldberg Machine/Assets/Scripts/Camera/CamTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamTrigger : MonoBehaviour {
+
+    public GameObject camList;
+    public int change;
+    private void OnTriggerEnter(Collider other)
+    {
+        camList.GetComponent<ChangeCam>().SwitchCam(change);
+    }
+}
---
+++ Rubegoldberg Machine/Assets/Scripts/Camera/ChangeCam.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCam : MonoBehaviour
+{
+    public Camera rotateCam;
+    public Camera boxCam0;
+    public Camera boxCam1;
+    public Camera boxCam2;
+
+    public void SwitchCam(int _cam)
+    {
+        rotateCam.enabled = false;
+        boxCam0.enabled = false;
+        boxCam1.enabled = false;
+        boxCam2.enabled = false;
+        switch (_cam)
+        {
+            case 0:
+                boxCam0.enabled = true;
+                break;
+            case 1:
+                boxCam1.enabled = true;
+                break;
+            case 2:
+                boxCam2.enabled = true;
+                break;
+            default:
+                rotateCam.enabled = true;
+                break;
+        }
+    }
+}
+
